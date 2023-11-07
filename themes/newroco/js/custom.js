72b859c1f4136aad17b8f5f976f67bf473d66b66
Oana Ursuleasa <oana.ursuleasa3199@gmail.com>
2023-11-07 10:27:28 +0200

    new file:   settings.php 	new file:   themes/menu.html.twig 	new file:   themes/newroco/css/img/check.png 	new file:   themes/newroco/css/img/download.png 	new file:   themes/newroco/css/img/ferenc-almasi-eYpcLDXHVb0-unsplash.jpg 	new file:   themes/newroco/css/img/header.png 	new file:   themes/newroco/css/img/header1.png 	new file:   themes/newroco/css/img/header2.png 	new file:   themes/newroco/css/img/img1.png 	new file:   themes/newroco/css/img/logo.png 	new file:   themes/newroco/css/img/map.png 	new file:   themes/newroco/css/img/romania-map.jpg 	new file:   themes/newroco/css/img/wave.png 	new file:   themes/newroco/css/img/web_dev_new.png 	new file:   themes/newroco/css/style_home.css 	new file:   themes/newroco/css/style_jobs.css 	new file:   themes/newroco/css/style_mahi.css 	new file:   themes/newroco/js/custom.js 	new file:   themes/newroco/logo.png 	new file:   themes/newroco/newroco.info.yml 	new file:   themes/newroco/newroco.libraries.yml 	new file:   themes/newroco/templates/layout/page.html.twig 	new file:   themes/newroco/templates/parts/content_bottom.html.twig 	new file:   themes/newroco/templates/parts/content_home.html.twig 	new file:   themes/newroco/templates/parts/content_jobs.html.twig 	new file:   themes/newroco/templates/parts/content_top.html.twig 	new file:   themes/newroco/templates/parts/footer.html.twig 	new file:   themes/newroco/templates/parts/header.html.twig

---
+++ themes/newroco/js/custom.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function () {
+
+/*   var path = window.location.pathname;
+  path = path.replace(/\/$/, "");
+  path = decodeURIComponent(path);
+
+  $(".menu a").each(function () {
+    var href = $(this).attr('href');
+
+    if (path.substring(0, href.length) === href) {
+      $(this).closest('li').addClass('active');
+    }
+  }); */
+});
