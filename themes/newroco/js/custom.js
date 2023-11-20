d3c887c33bca0e8f21d85ff730b4d6e0f600f4fd
Oana Ursuleasa <oana.ursuleasa3199@gmail.com>
2023-11-20 16:54:21 +0200

    technologies page

---
+++ themes/newroco/js/custom.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function($) {
+
+
+
+    $('.field__item a').on('click', function() {
+        $('.technology .field__item a').attr('target', '_blank');
+        $('.description_of_technologies .field__item a').attr('target', '_blank');
+        $('.segment_partners a').attr('target', '_blank');
+        $('.contact_footer2 a').attr('target', '_blank');
+
+     //add source_page to contact form
+        if ($('.field__item a').attr('href').includes('/contact')) {
+            var currentUrl = window.location.href;
+            sessionStorage.setItem('source_page', currentUrl);
+        }
+    });
+
+    $('#edit-field-source-page-wrapper').css('display','none');
+    var source_page= sessionStorage.getItem('source_page');
+    $('#edit-field-source-page-0-value').val(source_page);
+
+    //display of technology description
+    $('.technology2 .field__item a').removeAttr("href");
+    $('.description_of_technologies .field__item').attr('id', 'description_of_technologies');
+    $('.description_of_technologies').css('display','none');
+
+    $('.technology2 .field--name-body img').click(function() {
+        $('.description_of_technologies').show();
+        var index = $('.technology2 .field--name-body img').index(this);
+        $('.description_of_technologies .field__item').hide();
+        $('.technology2 .field--name-body img').css('filter','contrast(0.7) sepia(100%) hue-rotate(171deg) brightness(1)');
+        $description_of_technologies= $('.description_of_technologies .field__item').eq(index);
+
+        if ($description_of_technologies.length > 0) {
+            $description_of_technologies.show();
+            $('.technology2 .field--name-body img').eq(index).css('filter','none');
+            $('html, body').animate({
+                scrollTop: $('.title_technologies_page').offset().top
+            }, 1000);
+
+        }
+    });
+});
